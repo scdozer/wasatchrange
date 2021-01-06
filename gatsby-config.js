e1a84632da891b74bd072e69c325b0ca63febbcd
Scott Cardoza <scott@MacBook-Pro.localdomain>
2021-01-05 20:59:28 -0700

    analytics?

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,5 +50,11 @@ module.exports = {
         icon: `src/images/favicon-16x16.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["258030431"],
+      },
+    },
   ],
 };
